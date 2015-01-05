7c5af4e4e543272e9be75962084df8d15ddf7b06
Michael Ziwisky <mziwisky@instructure.com>
2015-01-05 00:27:52 -0600

    finished 'version 1'
    - made the different curves selectable
    - added a little blurb
    - styled the shit out of it

--- dragon.js
+++ dragon.js
@@ -3,42 +3,6 @@ var initial = {
   end: { x: 300, y: 0}
 };
 
-var rule = [
-  {
-    start: { x: 0, y: 0 },
-    end: { x: 150, y: -150 }
-  },{
-    start: { x: 300, y: 0 },
-    end: { x: 150, y: -150 }
-  }
-];
-
-var eveRule = [
-  {
-    start: { x: 0, y: 0 },
-    end: { x: 0, y: -150 }
-  },{
-    start: { x: 0, y: -150 },
-    end: { x: 150, y: 0 }
-  },{
-    start: { x: 300, y: 0 },
-    end: { x: 150, y: 0 }
-  }
-];
-
-var terDragonRule = [
-  {
-    start: { x: 0, y: 0 },
-    end: { x: 150, y: -150*Math.tan(Math.PI/6) }
-  },{
-    start: { x: 150, y: -150*Math.tan(Math.PI/6) },
-    end: { x: 150, y: 150*Math.tan(Math.PI/6) }
-  },{
-    start: { x: 150, y: 150*Math.tan(Math.PI/6) },
-    end: { x: 300, y: 0 }
-  }
-];
-
 // goldenDragon calculations based on http://ecademy.agnesscott.edu/~lriddle/ifs/heighway/goldenDragon.htm
 var goldenRatio = (1 + Math.sqrt(5)) / 2,
     gdR = Math.pow(1/goldenRatio, 1/goldenRatio),
@@ -47,36 +11,70 @@ var goldenRatio = (1 + Math.sqrt(5)) / 2,
     gdB = gdR * gdA,
     gdC = gdR2 * gdA,
     gdX = (Math.pow(gdA,2) + Math.pow(gdB,2) - Math.pow(gdC, 2)) / (2*gdA),
-    gdY = Math.sqrt(Math.pow(gdB,2) - Math.pow(gdX,2)),
-    goldenDragonRule = [
+    gdY = Math.sqrt(Math.pow(gdB,2) - Math.pow(gdX,2));
+
+var rules = [
   {
-    start: { x: 0, y: 0 },
-    end: { x: gdX, y: -gdY }
+    name: "Heighway Dragon",
+    rule: [{
+      start: { x: 0, y: 0 },
+      end: { x: 150, y: -150 }
+    },{
+      start: { x: 300, y: 0 },
+      end: { x: 150, y: -150 }
+    }]
   },{
-    start: { x: 300, y: 0 },
-    end: { x: gdX, y: -gdY }
+    name: "Dragon of Eve",
+    rule: [{
+      start: { x: 0, y: 0 },
+      end: { x: 0, y: -150 }
+    },{
+      start: { x: 0, y: -150 },
+      end: { x: 150, y: 0 }
+    },{
+      start: { x: 300, y: 0 },
+      end: { x: 150, y: 0 }
+    }]
+  },{
+    name: "Terdragon",
+    rule: [{
+      start: { x: 0, y: 0 },
+      end: { x: 150, y: -150*Math.tan(Math.PI/6) }
+    },{
+      start: { x: 150, y: -150*Math.tan(Math.PI/6) },
+      end: { x: 150, y: 150*Math.tan(Math.PI/6) }
+    },{
+      start: { x: 150, y: 150*Math.tan(Math.PI/6) },
+      end: { x: 300, y: 0 }
+    }]
+  },{
+    name: "Golden Dragon",
+    rule: [{
+      start: { x: 0, y: 0 },
+      end: { x: gdX, y: -gdY }
+    },{
+      start: { x: 300, y: 0 },
+      end: { x: gdX, y: -gdY }
+    }]
+  },{
+    name: "Koch",
+    rule: [{
+      start: { x: 0, y: 0 },
+      end: { x: 100, y: 0 }
+    },{
+      start: { x: 100, y: 0 },
+      end: { x: 150, y: -100*Math.sin(Math.PI/3) }
+    },{
+      start: { x: 150, y: -100*Math.sin(Math.PI/3) },
+      end: { x: 200, y: 0 }
+    },{
+      start: { x: 200, y: 0 },
+      end: { x: 300, y: 0 }
+    }]
   }
 ];
 
-var kochRule = [
-  {
-    start: { x: 0, y: 0 },
-    end: { x: 100, y: 0 }
-  },{
-    start: { x: 100, y: 0 },
-    end: { x: 150, y: -100*Math.sin(Math.PI/3) }
-  },{
-    start: { x: 150, y: -100*Math.sin(Math.PI/3) },
-    end: { x: 200, y: 0 }
-  },{
-    start: { x: 200, y: 0 },
-    end: { x: 300, y: 0 }
-  }
-];
-
-var transforms = goldenDragonRule.map(ruleToTransform);
-
-var lines = [initial];
+var transforms, lines;
 
 // KK, plans:
 // - curvy rendering so the path is more apparent
@@ -91,22 +89,21 @@ var lines = [initial];
 // - d3 animation stuff maybe?  if not the "stretch from original line" type thing
 //   that dragon of eve does, then maybe like fade in as red, then fade to white while
 //   original fades to nothingness
-//
-// k, rules will look like they do above, and executed like this:
-// 1) modify coordinate frame so line goes (0,0) -> (0,100)
-// 2) draw in "rule lines" in this new frame
-// 3) that's it.
+// - explore performance issues. drawing one long path rather than thousands of
+//   short ones, more efficient storage of `lines` array, maybe
+//   singleTransform/matrixVecMultiply during iterations can be faster (e.g.
+//   cuz vector's z is always 1), etc.
 
 var selector = '#draw-here',
     element = d3.select(selector);
 
 var svg = element.append('svg')
     .attr({
-      width: 600,
-      height: 600
+      width: '100%',
+      height: '100%'
     });
 var thing = svg.append('g')
-    .attr('transform', 'translate(150, 300)');
+    .attr('transform', 'translate(150, 350)');
 
 // mat:
 // [[A, B, C],
@@ -164,7 +161,7 @@ function ruleToTransform(rule) {
   return matrixMult(T, matrixMult(S, R));
 }
 
-function transform(line, matrix) {
+function singleTransform(line, matrix) {
   var start = matrixVecMult(matrix, [line.start.x, line.start.y, 1]),
       end = matrixVecMult(matrix, [line.end.x, line.end.y, 1]);
   return {
@@ -173,9 +170,9 @@ function transform(line, matrix) {
   };
 }
 
-function applyRules(line) {
-  return transforms.map(function(xform) {
-    return transform(line, xform);
+function applyTransforms(xforms, line) {
+  return xforms.map(function(xform) {
+    return singleTransform(line, xform);
   });
 }
 
@@ -183,7 +180,7 @@ function length(line) {
   return Math.sqrt(Math.pow(line.end.x - line.start.x, 2) + Math.pow(line.end.y - line.start.y, 2));
 }
 
-function render() {
+function render(lines) {
   var drawnLines = thing.selectAll('line').data(lines);
   drawnLines.enter()
     .append('line');
@@ -197,16 +194,68 @@ function render() {
   drawnLines.exit().remove();
 }
 
-render();
-
 function iterate() {
   var newLines = lines.reduce(function(result, line) {
-    applyRules(line).forEach(function(l) { result.push(l); });
+    applyTransforms(transforms, line).forEach(function(l) { result.push(l); });
     return result;
   }, []);
   lines = newLines;
-  render();
+  render(lines);
 }
 
-element.append('button').text('Iterate').on('click', iterate);
+function resetWithRule(rule) {
+  transforms = rule.rule.map(ruleToTransform);
+  lines = [initial];
+  render(lines);
+}
+
+function firstIterationFor(rule) {
+  return applyTransforms(rule.rule.map(ruleToTransform), initial);
+}
+
+svg.append('text')
+    .text('Click inside this box to grow the dragon') // TODO: make this say "...grow the <CURRENT DRAGON NAME>" instead
+    .attr('fill', '#2a2')
+    .attr('transform', 'translate(5 20)');
+svg.on('mousedown', function() {
+  d3.event.preventDefault(); // so the text doesn't get selected on double-clicks
+  iterate();
+});
+
+// TODO: this be a big mess.
+var sampleArea = d3.select('#samples');
+var samples = sampleArea.selectAll('div').data(rules);
+var samplesFrd = samples.enter()
+    .append('div')
+    .classed('sample', true)
+    .on('click', resetWithRule);
+var sampleSvgs = samplesFrd.append('svg')
+    .classed('sample-svg', true)
+    .on('mouseover', function() {
+      d3.select(this).classed('sample-svg--hovered', true);
+    })
+    .on('mouseout', function() {
+      d3.select(this).classed('sample-svg--hovered', false);
+    });
+sampleSvgs.append('g').attr('transform', 'translate(45 50) scale(0.3)')
+    .selectAll('line').data(function(d) { return firstIterationFor(d); })
+  .enter()
+    .append('line')
+    .attr({
+        'x1': function(d) { return d.start.x; },
+        'y1': function(d) { return d.start.y; },
+        'x2': function(d) { return d.end.x; },
+        'y2': function(d) { return d.end.y; },
+        'stroke': '#fff'
+      });
+sampleSvgs.append('text')
+    .text(function(d) { return d.name; })
+    .attr({
+      x: '100%',
+      y: '100%',
+      transform: 'translate(-5 -5)'
+    });
+// samplesFrd.append('span').text(function(d) { return d.name; }).classed('sample-name', true);
+
+resetWithRule(rules[0]);
 
